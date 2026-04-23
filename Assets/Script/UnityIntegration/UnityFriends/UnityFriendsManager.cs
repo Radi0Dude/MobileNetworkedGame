0622ab78acdd8b30479264700e36de40ef53da3b
Radi0Dude <tobias.fehn@hotmail.com>
2026-04-23 20:06:59 +0200

    Something new

--- Assets/Script/UnityIntegration/UnityFriends/UnityFriendsManager.cs
+++ Assets/Script/UnityIntegration/UnityFriends/UnityFriendsManager.cs
@@ -1,14 +1,15 @@
 using System.Threading.Tasks;
-using UnityEngine;
 using Unity.Services;
 using Unity.Services.Friends;
+using Unity.Services.Friends.Models;
 using Unity.Services.Friends.Notifications;
+using UnityEngine;
 
 public class UnityFriendsManager : MonoBehaviour
 {
     
     public async Task sendFriendRequest(string friendPlayerId)
     {
-        
+        await FriendsService.Instance.AddFriendAsync(friendPlayerId);
     }
 }
